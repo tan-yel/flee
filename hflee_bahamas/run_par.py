26fffe988ede1fc5f929ce5599a536e0cb282943
tan-yel <99021094+tan-yel@users.noreply.github.com>
2025-04-08 00:51:44 +0100

    set to 1

--- hflee_bahamas/run_par.py
+++ hflee_bahamas/run_par.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
   if SimulationSettings.spawn_rules["read_from_agents_csv_file"]:
       ig.ReadAgentsFromCSV(e, "%s/agents.csv" % input_csv_directory)
 
-  d = handle_refugee_data.RefugeeTable(csvformat="generic", data_directory=validation_data_directory, start_date=start_date, data_layout="data_layout.csv", population_scaledown_factor=SimulationSettings.optimisations["PopulationScaleDownFactor"], start_empty=SimulationSettings.spawn_rules["EmptyCampsOnDay0"])
+  d = handle_refugee_data.RefugeeTable(csvformat="generic", data_directory=validation_data_directory, start_date=start_date, data_layout="data_layout.csv", population_scaledown_factor=1, start_empty=SimulationSettings.spawn_rules["EmptyCampsOnDay0"])
   d.ReadL1Corrections("%s/registration_corrections.csv" % input_csv_directory)
 
   output_header_string = "Day,Date," #debug
